f70fe95c0a49d2378eae96652efa9e2c8ff37e67
Filip <dziubekfilip@gmail.com>
2022-11-20 01:28:30 +0100

    - create new models - create movie category list

---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using MovieLover.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieLover.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+    }
+}
