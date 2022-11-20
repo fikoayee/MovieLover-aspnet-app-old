a9679e2aa7f9d3cbe1689b4c3b2808df9d156ffd
Filip <dziubekfilip@gmail.com>
2022-11-20 08:46:11 +0100

    Create a SQLite Database with EF

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using MovieLover.Models;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
 
-namespace MovieLover.Data
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
+    public DbSet<Movie> Movies { get; set; }
 
-        }
+    public string DbPath { get; }
+
+    public AppDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "AppDb.db");
     }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //DbContext configuration
-builder.Services.AddDbContext<AppDbContext>();
 
 builder.Services.AddControllersWithViews();
 
